bff12bf19182972c6dc64358bcad09d18bf121e1
bwbhub <brice.bouhot@gmail.com>
2024-04-05 12:32:14 +0200

    update projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,8 +12,8 @@ import Carousel from "./functionnalities/Carousel";
 function importAll(r) {
   return r.keys().map(r);
 }
-const imagesKasa = importAll(
-  require.context("../assets/kasa", false, /\.(png|jpe?g|svg)$/),
+const imagesCineStation = importAll(
+  require.context("../assets/cine-station", false, /\.(png|jpe?g|svg)$/),
 );
 const imagesBook = importAll(
   require.context("../assets/book", false, /\.(png|jpe?g|svg)$/),
@@ -24,8 +24,17 @@ const imagesPort = importAll(
 
 const listProject = [
   {
+    title: "Cine-Station",
+    des: "I made this cinema app to work on my skills. You can navigate through movies, tv shows, and actors. you can signup (no email required so you can test it now !), sign in, update your password, add movies and tv shows to your favorites and leave reviews, you can also navigate through your favorites list, and do the same for the reviews you left.",
+    mission:
+      "Full-stack project, create a server, develop an API and connect to MongoDB, create a securised authentification system with hashed password and token, API calls to get the movies/tv shows.",
+    technologies: "MongoDB, Express, ReactJS, NodeJS",
+    images: imagesCineStation,
+    sourceCode: "https://github.com/bwbhub/cine-station",
+  },
+  {
     title: "Portfolio",
-    des: "This is the website you are on, it was the last project I had to do during my learning",
+    des: "This is the website you are on, it was the last project I had to do during my learning. You can see the skills I acquired, the projects I worked on, download my resume and contact me by email !",
     mission:
       "I crafted this project from scratch. I created simple and reusable components that can be reintroduced into other projects with minimal changes.",
     technologies: "ReactJs",
@@ -33,20 +42,11 @@ const listProject = [
     sourceCode: "https://github.com/bwbhub/Portfolio/tree/master",
   },
   {
-    title: "Kasa-Immobilier",
-    des: "This project was made during my time learning how to code. The CTO of this real estate rental agency wanted the entire website remodeled.",
-    mission:
-      "Front-end developer, create reusable ReactJs components, following a Figma model",
-    technologies: "ReactJs",
-    images: imagesKasa,
-    sourceCode: "https://github.com/bwbhub/kasa-immo",
-  },
-  {
     title: "Vieux grimoire",
     des: "This project was made during my time learning how to code. The owner of a few libraries wanted a website where users could add books and rate them.",
     mission:
-      "Back-end developer, create an Express server, develop an API and connect to MongoDB, create a securised authentification system with hashed password and token",
-    technologies: "Express, Node.Js, MongoDB",
+      "Back-end developer, create an Express server, develop an API and connect to MongoDB, create a securised authentification system with hashed password and token.",
+    technologies: "MongoDB, Express, Node.Js",
     images: imagesBook,
     sourceCode: "https://github.com/bwbhub/P7-Dev-Web-livres",
   },
